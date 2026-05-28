df4236f0282a1f682d8c3b4b80b9a198f8ffbf21
Andrey Qwebo <lao174@yandex.ru>
2026-05-28 11:21:07 +0300

    добавлена яндекс метрика

--- components/shared/html-wrapper.tsx
+++ components/shared/html-wrapper.tsx
@@ -1,4 +1,5 @@
 import localFont from "next/font/local"
+import Script from "next/script"
 import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import "@/app/globals.css"
@@ -27,10 +28,30 @@ export function HtmlWrapper({ children }: { children: React.ReactNode }) {
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body className={`${googleSans.variable} font-[family-name:var(--font-google-sans)] antialiased`}>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/62318476" style={{ position: "absolute", left: -9999 }} alt="" />
+          </div>
+        </noscript>
         <TooltipProvider>
           {children}
         </TooltipProvider>
         <Toaster position="top-right" richColors duration={2000} />
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js','ym');
+              ym(62318476, 'init', {clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
       </body>
     </html>
   )
